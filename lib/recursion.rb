d9ef825a73ce9c6cf1499718b090d9358a269a6a
Abdul Redd <abdul@turing.edu>
2023-12-06 01:20:47 -0500

    add readme; update parameter names

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -1,19 +1,19 @@
-def countdown(n)
-  return [0] if n <= 0
+def countdown(number)
+  return [0] if number <= 0
 
-  countdown(n - 1).unshift(n)
+  countdown(number - 1).unshift(number)
 end
 
-def get_sum(array)
-  return array[0] if array.length <= 1
+def get_sum(numbers)
+  return numbers[0] if numbers.length <= 1
 
-  array[0] + get_sum(array[1..-1])
+  numbers[0] + get_sum(numbers[1..-1])
 end
 
-def factorial(n)
-  return 1 if n <= 1
+def factorial(number)
+  return 1 if number <= 1
 
-  factorial(n - 1) * n
+  factorial(number - 1) * number
 end
 
 def reverse(string)
@@ -42,54 +42,54 @@ def upcase(string)
   string[0].upcase + upcase(string[1..-1])
 end
 
-def sum_of_digits(n)
-  return nil if n < 0
-  return n if n <= 1
+def sum_of_digits(number)
+  return nil if number < 0
+  return number if number <= 1
 
-  sum_of_digits(n - 1) + n
+  sum_of_digits(number - 1) + number
 end
 
-def in_stock?(array, target)
-  return false if array.empty?
-  return true if array[0] == target
+def in_stock?(inventory, item)
+  return false if inventory.empty?
+  return true if inventory[0] == item
 
-  in_stock?(array[1..-1], target)
+  in_stock?(inventory[1..-1], item)
 end
 
-def find_max(array)
-  return array[0] if array.length <= 1
+def find_max(numbers)
+  return numbers[0] if numbers.length <= 1
 
-  max = find_max(array[1..-1])
+  max = find_max(numbers[1..-1])
 
-  max > array[0] ? max : array[0]
+  max > numbers[0] ? max : numbers[0]
 end
 
-def number_of_zeroes(array)
-  return 0 if array.length == 0
+def number_of_zeroes(numbers)
+  return 0 if numbers.length == 0
 
-  count = array[0] == 0 ? 1 : 0
+  count = numbers[0] == 0 ? 1 : 0
 
-  number_of_zeroes(array[1..-1]) + count
+  number_of_zeroes(numbers[1..-1]) + count
 end
 
-def just_positives(array)
-  return 0 if array.length == 0
+def just_positives(numbers)
+  return 0 if numbers.length == 0
 
-  num = array[0] > 0 ? array[0] : 0
+  num = numbers[0] > 0 ? numbers[0] : 0
 
-  just_positives(array[1..-1]) + num
+  just_positives(numbers[1..-1]) + num
 end
 
-def number_of_occurrences(array, target)
-  return 0 if array.length == 0
+def number_of_occurrences(elements, target)
+  return 0 if elements.length == 0
 
-  count = array[0] == target ? 1 : 0
+  count = elements[0] == target ? 1 : 0
 
-  number_of_occurrences(array[1..-1], target) + count
+  number_of_occurrences(elements[1..-1], target) + count
 end
 
-def fibonacci(n)
-  return n if n <= 1
+def fibonacci(number)
+  return number if number <= 1
 
-  fibonacci(n - 2) + fibonacci(n - 1)
+  fibonacci(number - 2) + fibonacci(number - 1)
 end
